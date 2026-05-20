ae88d1da07d377e33d6b895d1a8b0b52269ecf93
Cavenber <cavenbercontacts@gmail.com>
2026-05-20 23:36:29 +0800

    0b02
    created toolbar

--- app/src/main/java/com/example/dumbasscalculatormk2/MainActivity.kt
+++ app/src/main/java/com/example/dumbasscalculatormk2/MainActivity.kt
@@ -2,13 +2,30 @@ package com.example.dumbasscalculatormk2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.NonNull
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+
+    lateinit var nv_side: NavigationView
+    lateinit var tv_snv: TextView
+
+    lateinit var toolbar: Toolbar
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,12 +34,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-            }
-        val program1: Button = findViewById(R.id.program1)
-        val intent = Intent(this, ArithmeticOperationActivity::class.java)
-
-        program1.setOnClickListener {
-            startActivity(intent)
         }
+
+        drawerLayout = findViewById(R.id.main)
+
+        nv_side = findViewById(R.id.nv_side)
+        tv_snv = findViewById(R.id.tv_snv)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nv_side.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { item ->
+            tv_snv.text=item.title // todo: modify this to allow for the change of activities
+            drawerLayout.closeDrawers()
+            true
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
